3496b5c37ebd104e549cc07bc71c52415e8a09fe
Ralph <devnymeria@gmail.com>
2024-05-21 12:51:05 -0500

    Add domain model Attributes

--- domain/model/src/main/java/co/com/wipro/model/book/Book.java
+++ domain/model/src/main/java/co/com/wipro/model/book/Book.java
@@ -1,14 +1,19 @@
 package co.com.wipro.model.book;
-import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
+
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
 }
