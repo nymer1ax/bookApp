d57831098d904cbf9f7a67ce574c8baeb9f38148
Ralph <devnymeria@gmail.com>
2024-05-21 13:07:14 -0500

    delete usecase

--- domain/usecase/src/main/java/co/com/wipro/usecase/deletebook/DeleteBookUseCase.java
+++ domain/usecase/src/main/java/co/com/wipro/usecase/deletebook/DeleteBookUseCase.java
@@ -1,6 +1,19 @@
 package co.com.wipro.usecase.deletebook;
 
+import co.com.wipro.model.book.Book;
+import co.com.wipro.model.book.gateways.BookRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
 @RequiredArgsConstructor
 public class DeleteBookUseCase {
+
+    private BookRepository bookRepository;
+    public void delete(Long id){
+        Optional<Book> existBook = bookRepository.getById(id);
+        if(existBook.isPresent()){
+            bookRepository.deleteById(id);
+        }
+    }
 }
