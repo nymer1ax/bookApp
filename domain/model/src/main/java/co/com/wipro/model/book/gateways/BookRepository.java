115d29c5072f0eaaca8aa13c627cb8d9aa28aa68
Ralph <devnymeria@gmail.com>
2024-05-21 12:57:54 -0500

    add gateway repository methods

--- domain/model/src/main/java/co/com/wipro/model/book/gateways/BookRepository.java
+++ domain/model/src/main/java/co/com/wipro/model/book/gateways/BookRepository.java
@@ -1,4 +1,14 @@
 package co.com.wipro.model.book.gateways;
 
+
+import co.com.wipro.model.book.Book;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepository {
+    List<Book> getAll();
+    Optional<Book> getById(Long id);
+    Book save(Book book);
+    void deleteById(Long id);
 }
