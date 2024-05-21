9d347fc394211b87fab5b8eb86d33203467d027f
Ralph <devnymeria@gmail.com>
2024-05-21 13:10:03 -0500

    Get All Usecase

--- domain/usecase/src/main/java/co/com/wipro/usecase/getallbook/GetAllBookUseCase.java
+++ domain/usecase/src/main/java/co/com/wipro/usecase/getallbook/GetAllBookUseCase.java
@@ -1,6 +1,16 @@
 package co.com.wipro.usecase.getallbook;
 
+import co.com.wipro.model.book.Book;
+import co.com.wipro.model.book.gateways.BookRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 public class GetAllBookUseCase {
+
+    private final BookRepository bookRepository;
+    public List<Book> getAllBooks() {
+        return bookRepository.getAll();
+    }
 }
