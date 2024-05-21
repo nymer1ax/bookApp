0678a0438255f30024ca9bf6241e8925d9b32d24
Ralph <devnymeria@gmail.com>
2024-05-21 15:42:00 -0500

    Add basic auth

--- infrastructure/entry-points/api-rest/src/main/java/co/com/wipro/api/config/CorsConfig.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/wipro/api/config/CorsConfig.java
@@ -1,10 +1,8 @@
 package co.com.wipro.api.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -16,18 +14,16 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter(@Value("${cors.allowed-origins}") String origins) {
+    public CorsFilter corsFilter(@Value("${cors.allowed-origins}") String origins) {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(origins.split(",")));
-        config.setAllowedMethods(Arrays.asList("POST", "GET")); // TODO: Check others required methods
+        config.setAllowedOrigins(List.of(origins.split(","))); // Assuming comma-separated origins
+        config.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE")); // Check other required methods
         config.setAllowedHeaders(List.of(CorsConfiguration.ALL));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        final FilterRegistrationBean<CorsFilter> corsFilter = new FilterRegistrationBean<>(new CorsFilter(source));
-        corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return corsFilter;
+        return new CorsFilter(source);
     }
 }
